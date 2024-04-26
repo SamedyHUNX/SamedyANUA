3000d1077880e2d8bfa5d87700dccb5727c5b88e
samedy-leader <fordancestyle@gmail.com>
2024-04-26 17:13:56 +0700

    3

---
+++ membership_js.js
@@ -0,0 +1,23 @@
+// const carouselSlides = document.querySelectorAll('.carousel-slide');
+// const navigationDots = document.querySelectorAll('.dot');
+
+// let currentSlide = 0; // Keeps track of the current slide
+
+// function updateCarousel() {
+// carouselSlides.forEach((slide) => slide.classList.remove('active')); // Remove active class from all slides
+// navigationDots.forEach((dot) => dot.classList.remove('active')); // Remove active class from all dots
+
+// carouselSlides[currentSlide].classList.add('active'); // Add active class to current slide
+// navigationDots[currentSlide].classList.add('active'); // Add active class to current dot
+// }
+
+// function changeSlide(slideIndex) {
+// currentSlide = slideIndex - 1; // Adjust for zero-based indexing
+// updateCarousel();
+// }
+
+// navigationDots.forEach((dot, index) => {
+// dot.addEventListener('click', () => {
+//     changeSlide(index + 1); // Adjust for zero-based indexing
+// });
+// });
